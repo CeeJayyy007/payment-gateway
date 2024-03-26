a12b0f41fc77914a45095db1a66fb9e984cc0995
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2024-03-26 07:02:44 -0400

    chore: add data view to success page

--- src/pages/success-page.jsx
+++ src/pages/success-page.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom"; // If using React Router
+import { Link, useLocation } from "react-router-dom"; // If using React Router
 import { CheckCircledIcon } from "@radix-ui/react-icons"; // Import the CheckIcon component from Radix Icons
 
 const SuccessPage = () => {
+  // Get the transaction details from the payment gateway
+  const location = useLocation();
+  const data = location.state;
+
   const transactionDetails = {
     amount: 100,
     currency: "USD",
-    // Add more transaction details as needed
   };
 
   return (
@@ -23,6 +26,12 @@ const SuccessPage = () => {
       <Link to="/" className="text-blue-500 underline hover:text-blue-700">
         Return to Home
       </Link>{" "}
+      <div className="mt-[50px] flex flex-col items-center justify-center text-xs">
+        <p>You submitted the following values:</p>
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4 text-xs">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      </div>
     </div>
   );
 };
